19b1d3d4acad489bbf3c5e125bf88cb5a559ad6d
Duy Dev <duy99.uit@gmail.com>
2022-03-10 17:48:23 +0700

    done layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,127 @@
 import "./App.scss";
 function App() {
-  return <div className="App">hii</div>;
+  return (
+    <div className="trello">
+      <div className="navbar app">Trello rep1:1</div>
+      <div className="navbar boards">Board Bart</div>
+      <div className="board-columns">
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1646900069517-955d7253efa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+                alt="trello"
+              />
+              Trello demo content list
+            </li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+          </ul>
+          <footer>Add new content</footer>
+        </div>
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1640622307877-1e40352b9a48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                alt="trello"
+              />
+              Trello demo content list
+            </li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+          </ul>
+          <footer>Add new content</footer>
+        </div>
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1646617747575-89f8eb728264?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                alt="trello"
+              />
+              Trello demo content list
+            </li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+          </ul>
+          <footer>Add new content</footer>
+        </div>
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1646900069517-955d7253efa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+                alt="trello"
+              />
+              Trello demo content list
+            </li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+          </ul>
+          <footer>Add new content</footer>
+        </div>
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1646900069517-955d7253efa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+                alt="trello"
+              />
+              Trello demo content list
+            </li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+          </ul>
+          <footer>Add new content</footer>
+        </div>
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1646900069517-955d7253efa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+                alt="trello"
+              />
+              Trello demo content list
+            </li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+            <li>Trello demo content list</li>
+          </ul>
+          <footer>Add new content</footer>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+export const imageAssets =[
+    'https://images.unsplash.com/photo-1646900069517-955d7253efa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
+    'https://images.unsplash.com/photo-1646617747575-89f8eb728264?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    'https://images.unsplash.com/photo-1640622307877-1e40352b9a48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+
+]
