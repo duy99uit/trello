3f59e84b43c2662d53cd1e47cc24631f0ba04a99
Duy Dev <duy99.uit@gmail.com>
2022-03-10 21:01:37 +0700

    re-structure project

---
+++ src/components/AppBar/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+function AppBar() {
+  return <div className="navbar-app">Trello rep1:1</div>;
+}
+
+export default AppBar;
---
+++ src/components/BoardBar/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+function BoardBar() {
+  return <div className="navbar-boards">Board Bart</div>
+}
+
+export default BoardBar;
