af715d2f725c23134f496cf4d9457e1640204506
Duy Dev <duy99.uit@gmail.com>
2022-04-11 00:39:22 +0700

    update

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -2,7 +2,51 @@ import React from "react";
 import "./style.scss";
 
 function AppBar() {
-  return <div className="navbar-app">Trello rep1:1</div>;
+  return (
+    <>
+      {/* <div className="navbar-app">Trello rep1:1</div> */}
+      <header className="masthead">
+        <div className="boards-menu">
+          <button className="boards-btn btn">
+            <i className="fa fa-trello boards-btn-icon"></i>Boards
+          </button>
+
+          <div className="board-search">
+            <input
+              type="search"
+              className="board-search-input"
+              aria-label="Board Search"
+            />
+            <i className="fa fa-search search-icon" aria-hidden="true"></i>
+          </div>
+        </div>
+
+        <div className="logo">
+          <h3>
+            <i className="fa fa-trello logo-icon" aria-hidden="true"></i>Trello
+          </h3>
+        </div>
+
+        <div className="user-settings">
+          <button className="user-settings-btn btn" aria-label="Create">
+            <i className="fa fa-plus" aria-hidden="true"></i>
+          </button>
+
+          <button className="user-settings-btn btn" aria-label="Information">
+            <i className="fa fa-info-circle" aria-hidden="true"></i>
+          </button>
+
+          <button className="user-settings-btn btn" aria-label="Notifications">
+            <i className="fa fa-bell" aria-hidden="true"></i>
+          </button>
+
+          <button className="user-settings-btn btn" aria-label="User Settings">
+            <i className="fa fa-user-circle" aria-hidden="true"></i>
+          </button>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default AppBar;
--- src/components/BoardBar/index.js
+++ src/components/BoardBar/index.js
@@ -2,7 +2,37 @@ import React from "react";
 import "./style.scss";
 
 function BoardBar() {
-  return <div className="navbar-boards">Board Bart</div>
+  return (
+    <>
+      {/* <div className="navbar-boards">Board Bart</div> */}
+      <section className="board-info-bar">
+        <div className="board-controls">
+          <button className="board-title btn">
+            <h4>Web Development</h4>
+          </button>
+
+          <button className="star-btn btn" aria-label="Star Board">
+            <i className="fa fa-star" aria-hidden="true"></i>
+          </button>
+
+          <button className="personal-btn btn">Personal</button>
+
+          <button className="private-btn btn">
+            <i
+              className="fa fa-briefcase private-btn-icon"
+              aria-hidden="true"
+            ></i>
+            Private
+          </button>
+        </div>
+
+        <button className="menu-btn btn">
+          <i className="fa fa-ellipsis-h menu-btn-icon" aria-hidden="true"></i>
+          Show Menu
+        </button>
+      </section>
+    </>
+  );
 }
 
 export default BoardBar;
